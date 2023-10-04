8c90eb6db681f0f856ab870dec6fd95405143dcb
JuneKoo Kang <engineerjkk@naver.com>
2023-10-04 17:05:34 +0900

    Add files via upload

--- 로봇암/Server(로봇암)/TestCameraTrajectories.py
+++ 로봇암/Server(로봇암)/TestCameraTrajectories.py
@@ -3,6 +3,22 @@ from pymycobot.genre import Angle, Coord
 from pymycobot import PI_PORT, PI_BAUD
 import time 
 import pickle
+import socket 
+
+HOST = '0.0.0.0'
+PORT = 12345
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((HOST,PORT))
+
+server_socket.listen()
+print("START, PORT : ",PORT)
+
+client_socket, addr = server_socket.accept()
+
 
 mc = MyCobot(PI_PORT,PI_BAUD)
 while mc.is_power_on() != 0:
@@ -21,6 +37,15 @@ mc.release_all_servos()
 with open("traj.pkl", "rb") as f:
     trajectories = pickle.load(f)
 
+#Initial 6DoF
+x=trajectories[0][0]
+y=trajectories[0][1]
+z=trajectories[0][2]
+roll=trajectories[0][3]
+pitch=trajectories[0][4]
+yaw=trajectories[0][5]
+
+mc.send_coords(trajectories[0],30,1)
 mc.send_coords(trajectories[0],30,1)
 while True:
     res = mc.get_coords()
@@ -30,9 +55,17 @@ while True:
 print("To restore trajectories, press ANY key")
 input()
 
+
 for pos in trajectories:
-    mc.send_coords(pos, 20, 1)
-    # time.sleep(0.1)
+    mc.send_coords(pos, 5, 1)
+    # pos = pos - [-136.9, -27.9, 243.0, -87.33, -34.99, -114.91]
+    message=f"{pos[0]-x},{pos[1]-y},{pos[2]-z},{pos[3]-roll},{pos[4]-pitch},{pos[5]-yaw}"
+    client_socket.sendall(message.encode())
+    print("Sending data : ", message)
+
+    time.sleep(0.1)
 
 
 print("finished process")
+client_socket.close()
+server_socket.close()
